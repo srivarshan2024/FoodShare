9565beb9369e593d20402d3d4b41fe1667df299c
Varshan <test@example.com>
2026-02-14 08:44:17 +0530

    Initial FoodShare MERN Project

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();s
+  if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
